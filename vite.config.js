60bc3c2989977235df203bce7bc11d986a6073b4
Asgar Rashid <asgar.raza013@gmail.com>
2025-10-22 16:42:28 +0530

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/potfolio/' : '/',
-}))
+  base: process.env.VITE_BASE_PATH || "/potfolio"
+})
