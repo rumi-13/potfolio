d1890548cc56eaad41dbefd038ba6f4ee548b80d
Asgar Rashid <asgar.raza013@gmail.com>
2026-04-16 13:44:31 +0530

    fix path

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import {
   createRoutesFromElements,
   RouterProvider,
   Route,
-} from 'react-router-dom';
-import './App.css';
-import Layout from './Layout';
-import Skills from './components/Skills';
-import Home from './components/Home';
-import Education from './components/Education';
-import About from './components/About';
+} from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout";
+import Skills from "./components/Skills";
+import Home from "./components/Home";
+import Education from "./components/Education";
+import About from "./components/About";
 
 function App() {
   const router = createBrowserRouter(
@@ -18,10 +18,13 @@ function App() {
         <Route index element={<Home />} />
         <Route path="Home" element={<Home />} />
         <Route path="Skills" element={<Skills />} />
-        <Route path="Education" element={<Education/>}/>
-         <Route path="About" element={<About />} />
-      </Route>
-    )
+        <Route path="Education" element={<Education />} />
+        <Route path="About" element={<About />} />
+      </Route>,
+    ),
+    {
+      basename: "/portfolio", // 🔥 ADD THIS
+    },
   );
 
   return <RouterProvider router={router} />;
